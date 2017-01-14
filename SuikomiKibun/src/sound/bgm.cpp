dc741e14aa00943583e9c551adc25af26ad2306f
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2017-01-14 22:04:50 +0900

    スタート画面にbgmを追加

--- SuikomiKibun/src/sound/bgm.cpp
+++ SuikomiKibun/src/sound/bgm.cpp
@@ -6,10 +6,22 @@
  */
 
 #include "bgm.h"
-#include <stdio.h>
+
+//コンストラクタ
+Bgm::Bgm() {
+	for (int i = 0; i < Bgm::BgmStructNum; i++) {
+		distance_[i] = 10.0;
+		pbgm_[i] = NULL;
+	}
+}
+
+//デストラクタ
+Bgm::~Bgm() {
+	Stop();
+}
 
 //Bgmを鳴らす
-void Bgm::Play(Bgm::BgmStruct sound) {
+void Bgm::Play(BgmStruct sound) {
 	if (sound >= 0 && sound < BgmStructNum)
 		delete pbgm_[sound];
 	switch (sound) {
@@ -23,9 +35,16 @@ void Bgm::Play(Bgm::BgmStruct sound) {
 		pbgm_[sound] = new Sound("sound/result.wav");
 		break;
 	default:
-		fprintf(stderr, "そのBGMは存在しません");
+		uErrorOut(__FILE__, __func__, __LINE__, "そのBGMは存在しません");
 		break;
 	}
+}
+
+void Bgm::Play(BgmStruct sound, float distance) {
+	//BGM開始
+	Play(sound);
+	//距離設定
+	set_distance(sound, distance);
 }
 
 //Bgmを更新
@@ -33,7 +52,7 @@ void Bgm::Update() {
 	for (int i = 0; i < Bgm::BgmStructNum; i++) {
 		if (pbgm_[i] == NULL)
 			continue;
-		pbgm_[i]->SetSourceToListenerFront(10); //音源をリスナーに同期
+		pbgm_[i]->SetSourceToListenerFront(distance_[i]); //音源をリスナーに同期
 		pbgm_[i]->Stream(); //ループさせる
 	}
 }
@@ -46,6 +65,11 @@ void Bgm::Stop() {
 	}
 }
 
-Bgm::~Bgm() {
-	Stop();
+//setter
+void Bgm::set_distance(BgmStruct sound, float d) {
+	if (sound >= 0 && sound < BgmStructNum)
+		distance_[sound] = d;
+	else
+		uErrorOut(__FILE__, __func__, __LINE__, "そのBGMは存在しません");
 }
+
--- SuikomiKibun/src/sound/bgm.h
+++ SuikomiKibun/src/sound/bgm.h
@@ -16,12 +16,22 @@ public:
 	enum BgmStruct {
 		kStartBgm = 0, kGameBgm, kResultBgm, BgmStructNum
 	};
-	void Play(BgmStruct sound); //BGMを流す
+
+	Bgm();
+	~Bgm();
+	//BGMを流す
+	void Play(BgmStruct sound);
+	void Play(BgmStruct sound, float distance);
+
 	void Update(); //更新(毎フレーム呼び出す)
 	void Stop(); //すべてのBGMを止める
-	~Bgm();
+
+	//setter
+	void set_distance(BgmStruct sound, float d);
+
 private:
 	Sound *pbgm_[BgmStructNum]; //Bgm
+	float distance_[BgmStructNum]; //リスナーからの距離
 };
 
 #endif /* SOUND_BGM_H_ */
