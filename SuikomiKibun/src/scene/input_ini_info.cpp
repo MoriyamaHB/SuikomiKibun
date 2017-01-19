2aeb6827164f49af00c640152d54195f4b508558
MoriyamaHB <mhrbykm122@yahoo.co.jp>
2017-01-19 22:02:24 +0900

    入力画面の作成完了

--- SuikomiKibun/src/scene/input_ini_info.cpp
+++ SuikomiKibun/src/scene/input_ini_info.cpp
@@ -9,47 +9,41 @@ InputIniInfo::InputIniInfo(ISceneChanger* changer, SceneParam param) :
 	} else {
 		font_.FaceSize(90);
 	}
+	//std::stringの初期化(空の状態でシーン遷移しようとするとコアダンプするため)
+	data.client_name = "no";
+	data.server_ip = "no";
+	//入力終了フラグセット
+	is_finish_input = false;
+	//入力開始
+	thread_ = new boost::thread(&InputIniInfo::ThreadInput, this);
 }
 
 //デストラクタ
 InputIniInfo::~InputIniInfo() {
+	//スレッド終了まで待機←これやると終わらないので強制終了
+//	if (thread_->joinable())
+//		thread_->join();
+	//スレッド削除
+	delete thread_;
 }
 
 //更新
 void InputIniInfo::Update() {
-	//フレームカウント
-	static int frame_num = 0;
-	frame_num++;
-
-	switch (frame_num) {
-	case 1: //1フレーム目
-		u3Dto2D();
-		if (!font_.Error()) {
-			glColor4fv(uColor4fv_orange);
-			glRasterPos2f(10, 200);
-			font_.Render("端末で情報を入力してください");
-		}
-		u2Dto3D();
-		break;
-	case 2:  //2フレーム目
-		std::cout << "サーバーなら's',クライアントなら'c'を入力してください:";
-		std::cin >> data.s_or_c;
-		if (data.s_or_c == 'c') {
-			std::cout << "サーバーのPC名:";
-			std::cin >> data.server_ip;
-			std::cout << "ポート番号(1人目:31601,2人目:31602):";
-			std::cin >> data.port;
-		} else if (data.s_or_c == 's') {
-			data.server_ip = "localhost";
-			data.port = 31600;
-		}
-		std::cout << "あなたの名前:";
-		std::cin >> data.client_name;
+	//入力終了で次のシーンに進む
+	if (is_finish_input) {
 		SceneParam par;
 		par.input_ini_info_data = data;
 		scene_changer_->ChangeScene(kSceneGame, par);
-		break;
 	}
+
+	//案内表示
+	u3Dto2D();
+	if (!font_.Error()) {
+		glColor4fv(uColor4fv_orange);
+		glRasterPos2f(10, 200);
+		font_.Render("端末で情報を入力してください");
+	}
+	u2Dto3D();
 }
 
 //描画
@@ -58,5 +52,18 @@ void InputIniInfo::Draw() const {
 
 //入力スレッド
 void InputIniInfo::ThreadInput() {
-
+	std::cout << "サーバーなら's',クライアントなら'c'を入力してください:";
+	std::cin >> data.s_or_c;
+	if (data.s_or_c == 'c') {
+		std::cout << "サーバーのPC名:";
+		std::cin >> data.server_ip;
+		std::cout << "ポート番号(1人目:31601,2人目:31602):";
+		std::cin >> data.port;
+	} else if (data.s_or_c == 's') {
+		data.server_ip = "localhost";
+		data.port = 31600;
+	}
+	std::cout << "あなたの名前:";
+	std::cin >> data.client_name;
+	is_finish_input = true;	//入力終了
 }
--- SuikomiKibun/src/scene/input_ini_info.h
+++ SuikomiKibun/src/scene/input_ini_info.h
@@ -1,6 +1,8 @@
 #ifndef SUIKOMIKIBUN_SCENE_SELECT_H_
 #define SUIKOMIKIBUN_SCENE_SELECT_H_
 
+#include <boost/thread.hpp>
+
 #include "scene_mgr.h"
 #include "../input/input.h"
 #include "../util/output_display.h"
@@ -10,6 +12,8 @@ class InputIniInfo: public BaseScene {
 private:
 	InputIniInfoData data;
 	FTPixmapFont font_;
+	boost::thread *thread_;
+	bool is_finish_input;
 
 	void ThreadInput();
 public:
