7b04b82d2aed9b964c4ce57a674ea3cc7f1085ad
mi1per <c51506039@tokushima-u.ac.jp>
2017-01-27 16:46:56 +0900

    調整

--- SuikomiKibun/src/player/player.cpp
+++ SuikomiKibun/src/player/player.cpp
@@ -240,7 +240,7 @@ void Player::Update(double angle, StageMap* map, int color_judge1,
 		if (sphere_tekibody1_ == delete_body_)
 			delete_body_ = delete_body2_;
 		if (delete_body_ != NULL) {
-			for (i = world_->getNumCollisionObjects() - 1; i > 141; i--) {
+			for (i = world_->getNumCollisionObjects() - 1; i > 109; i--) {
 				obj = world_->getCollisionObjectArray()[i];
 				body = btRigidBody::upcast(obj);
 				if (delete_body_ == body) {
